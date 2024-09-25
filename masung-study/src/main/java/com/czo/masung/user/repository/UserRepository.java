8f8afee2f5698eefd8b696613debc082072debc6
김구 <skk3010g@gmail.com>
2024-09-25 13:23:59 +0900

    mybatis 버전 업데이트, 회원정보 상세보기 기능 구현

--- masung-study/src/main/java/com/czo/masung/user/repository/UserRepository.java
+++ masung-study/src/main/java/com/czo/masung/user/repository/UserRepository.java
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository {
 	public List<UserVO> getList(PageRequestDTO pageRequestDTO);
 	public int getTotalCount(PageRequestDTO pageRequestDTO);
-	public Optional<UserVO> getRead(String uid);
-	public Optional<UserVO> getRead_uuid(String uuid);
-	public int remove(String uid);
-	public int  modify(UserVO member);
-	public int  modify_uuid(UserVO member);
-	public int  insert(UserVO member);
+	public Optional<UserVO> getRead(String user_id);
+	public Optional<UserVO> getRead_uuid(String user_uuid);
+	public int remove(String user_id);
+	public int  modify(UserVO user);
+	public int  modify_uuid(UserVO user);
+	public int  insert(UserVO user);
 }
